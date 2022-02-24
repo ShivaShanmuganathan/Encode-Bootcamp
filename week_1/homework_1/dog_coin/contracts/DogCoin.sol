6f7e61d30d496924e94130a21f6320d73d2a9e2e
Shiva <shanshiva@outlook.com>
2022-02-24 19:19:17 +0530

    ERC20 Template Added for DoCoin

---
+++ week_1/homework_1/dog_coin/contracts/DogCoin.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    
+    constructor() ERC20("DogCoin", "DC") {
+
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+
+}
