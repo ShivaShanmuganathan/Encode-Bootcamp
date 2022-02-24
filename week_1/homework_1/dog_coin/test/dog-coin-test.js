f78043518f2489a01308d28572e45a85d0840b04
Shiva <shanshiva@outlook.com>
2022-02-25 01:37:31 +0530

    update holders when minting and transferring

--- week_1/homework_1/dog_coin/test/dog-coin-test.js
+++ week_1/homework_1/dog_coin/test/dog-coin-test.js
@@ -2,6 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Dog Coin Test", function () {
+
   it("Should return the list of token returns", async function () {
     
     const DogCoin = await ethers.getContractFactory("DogCoin");
@@ -9,10 +10,18 @@ describe("Dog Coin Test", function () {
     await dog_coin.deployed();
     
     [addr1, addr2] = await hre.ethers.getSigners();
+    
     await dog_coin.mint(addr1.address, ethers.utils.parseEther("100"));
 
-    console.log(await dog_coin.getHolders());
-
+    await dog_coin.connect(addr1).transfer(addr2.address, ethers.utils.parseEther("50"));
     
+    console.log("Balance of Sender", parseFloat(ethers.utils.formatEther(await dog_coin.balanceOf(addr1.address))));
+    console.log("Balance of Receiver", parseFloat(ethers.utils.formatEther(await dog_coin.balanceOf(addr2.address))));
+
+    console.log(await dog_coin.getHolders());
+    console.log(addr1.address);
+    console.log(addr2.address);
+    
+
   });
 });
