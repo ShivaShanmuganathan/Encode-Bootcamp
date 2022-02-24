595a308537b3e46777e0c778fffd71b6824b7875
Shiva <shanshiva@outlook.com>
2022-02-24 21:04:40 +0530

    upadte holders list when minting and transferring

--- week_1/homework_1/dog_coin/contracts/DogCoin.sol
+++ week_1/homework_1/dog_coin/contracts/DogCoin.sol
@@ -5,7 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "hardhat/console.sol";
 
 contract DogCoin is ERC20 {
-    
+    mapping(address => uint256) private _balances;
+    uint256 private _totalSupply;
     address[] public holders;
     uint[] public holders2;
     
@@ -22,7 +23,41 @@ contract DogCoin is ERC20 {
         return holders;
 
     }
-    
 
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return _balances[account];
+    }
+
+
+    function _mint(address account, uint256 amount) internal virtual override{
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+
+        _afterTokenTransfer(address(0), account, amount);
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal virtual override{
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+
+        _beforeTokenTransfer(from, to, amount);
+
+        uint256 fromBalance = _balances[from];
+        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
+        unchecked {
+            _balances[from] = fromBalance - amount;
+        }
+        _balances[to] += amount;
+        holders.push(to);
+
+        emit Transfer(from, to, amount);
+
+        _afterTokenTransfer(from, to, amount);
+    }
 
 }
