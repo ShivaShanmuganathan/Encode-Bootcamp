668a29decaa191e39bbc6458dd23c7c3b762c98f
Shiva <shanshiva@outlook.com>
2022-02-25 23:37:13 +0530

    Added Test for transferFrom function
    1. Test includes approve and transferFrom functionality
    2. User_Added event is expected to not be emmitted when transferring ERC-20 tokens to non-unique user.

--- week_1/homework_1/dog_coin/test/dog-coin-test.js
+++ week_1/homework_1/dog_coin/test/dog-coin-test.js
@@ -22,6 +22,12 @@ describe("Dog Coin Test", function () {
     await expect(dog_coin.connect(addr1).mint(addr1.address, ethers.utils.parseEther("100"))).to.not.emit(dog_coin, "User_Added").withArgs(addr1.address);
     
     await expect(dog_coin.connect(addr1).transfer(addr2.address, ethers.utils.parseEther("50"))).to.emit(dog_coin, "User_Added").withArgs(addr2.address);
+    
+    await dog_coin.connect(addr1).approve(addr2.address, ethers.utils.parseEther("10"));
+    
+    console.log("Allowance Of", parseFloat(ethers.utils.formatEther(await dog_coin.connect(addr2).allowance(addr1.address, addr2.address))));
+    
+    await expect(dog_coin.connect(addr2).transferFrom(addr1.address, addr2.address, ethers.utils.parseEther("10"))).to.not.emit(dog_coin, "User_Added").withArgs(addr2.address);
 
     await expect(dog_coin.connect(addr3).transfer(addr4.address, ethers.utils.parseEther("100"))).to.emit(dog_coin, "User_Removed").withArgs(addr3.address).to.emit(dog_coin, "User_Added").withArgs(addr4.address);
     
