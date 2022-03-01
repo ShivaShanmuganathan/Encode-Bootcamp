5f92035028931ab34e41dc61aaceac2c26be4619
Shiva <shanshiva@outlook.com>
2022-03-01 22:59:08 +0530

    Added Test for deploy proposal factory and new proposal contract created

--- week_1/homework_3/test/sample-test.js
+++ week_1/homework_3/test/sample-test.js
@@ -1,19 +1,96 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Proposal Factory", function () {
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  before(async function () {
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();          
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
+  
+  it("Should Deploy Proposal Factory & Create New Proposal Contracts", async function () {
+    
+    const ProposalFactory = await ethers.getContractFactory("ProposalFactory");
+    const Proposal = await ethers.getContractFactory("Proposal");
+    
+    const proposal_factory = await ProposalFactory.deploy();
+    await proposal_factory.deployed();
+    
+
+    proposal = await proposal_factory.createProposal(3, "Hello World Is Awesome", Math.floor(Date.now() / 1000 + 3600));
+
+    console.log("Deployed Proposal Address", await proposal_factory.getDeployedProposals());
+    proposed_contract = await Proposal.attach((await proposal_factory.getDeployedProposals())[0]);
+    
+  });
+
+  it("Should return description of proposal in the new proposal contract", async function () {  
+
+    const return_value = (await proposed_contract.getSummary());
+    console.log(return_value.description.toString());
+
+  });
+
+  it("Should check the proposer of the new proposal contract", async function () {  
+    
+    const return_value = (await proposed_contract.getSummary());
+    console.log("Proposer Address", return_value.proposer.toString());
+    console.log("Owner Address", owner.address)
+
+  });
+
+  it("Should set voting in the new proposal contract", async function () {  
+    // enum returns 1 because VOTING is in 1 position in enum
+    await proposed_contract.setVote();
+    const return_value = (await proposed_contract.getSummary());
+    console.log(return_value.status.toString());
+
+  });
+
+  it("Should upvote in the new proposal contract", async function () {  
+    
+  
+    await proposed_contract.connect(addr1).upvote();
+    await proposed_contract.connect(addr2).upvote();
+    const return_value = (await proposed_contract.getSummary());
+    console.log("Approval Count", return_value.approvalCount.toString());
+
+  });
+
+  it("Should downvote in the new proposal contract", async function () {  
+    
+    await proposed_contract.connect(addr3).downvote();
+    await proposed_contract.connect(addr4).downvote();
+    const return_value = (await proposed_contract.getSummary());
+    console.log("Disapproval Count", return_value.disApprovalCount.toString());
+
+  });
+
+  // it("Should upvote in the new proposal contract & move to ACCEPTED status", async function () {  
+    
+  //   // enum returns 2 because ACCEPTED is in 2 position in enum
+  //   await proposed_contract.connect(addr1).upvote();
+  //   await proposed_contract.connect(addr2).upvote();
+  //   await proposed_contract.connect(addr3).upvote();
+  //   // await proposed_contract.connect(addr4).downvote();
+  //   const return_value = (await proposed_contract.getSummary());
+  //   console.log("Status Of Proposal", return_value.status.toString());
+
+  // });
+
+  it("Should downvote in the new proposal contract & move to REJECTED status", async function () {  
+    
+    // enum returns 3 because REJECTED is in 3 position in enum
+    await proposed_contract.connect(addr1).downvote();
+    await proposed_contract.connect(addr2).downvote();
+    await proposed_contract.connect(addr3).downvote();
+    // await proposed_contract.connect(addr4).downvote();
+    const return_value = (await proposed_contract.getSummary());
+    console.log("Status Of Proposal", return_value.status.toString());
+
+  });
+
+
+  
 });
