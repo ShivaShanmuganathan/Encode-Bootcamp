8fc06a7a1777b2a87d89da75ea9c9711ef6ffbfc
Shiva <shanshiva@outlook.com>
2022-03-15 21:45:34 +0530

    remove comments

--- week_2/homework_10/contracts/Assembly.sol
+++ week_2/homework_10/contracts/Assembly.sol
@@ -15,37 +15,7 @@ contract Assembly {
             let x := mload(0x40) // get empty storage location
             mstore ( x, value )
             a := x
-
-			// Creating a new variable 'c'
-			// Calculate the sum of 'a+16'
-			// with the 'add' opcode
-			// assign the value to the 'c'
-			// let c := msg.value
-
-			// Use 'mstore' opcode to
-			// store 'c' in memory
-			// at memory address 0x80
-			// mstore(0x80, c)
-			{
-
-				// Creating a new variable'
-				// Calculate the sum of 'sload(c)+12'
-				// means values in variable 'c'
-			// with the 'add' opcode
-			// assign the value to 'd'
-				// let d := add(sload(c), 12)
-
-				// assign the value of 'd' to 'b'
-				// b := d
-
-			// 'd' is deallocated now
-			}
-	
-			// Calculate the sum of 'b+c' with the 'add' opcode
-			// assign the value to 'b'
-			// b := add(b, c)
-
-		// 'c' is deallocated here
+			
 		}
 	}
 }
